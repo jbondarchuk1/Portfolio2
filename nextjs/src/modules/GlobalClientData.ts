475c37c1ea0ea672252249deecbdb087b7bceb4a
Jason Bondarchuk <jason.r.bondarchuk@gmail.com>
2024-12-09 00:01:56 -0500

    Update GlobalClientData.ts

--- nextjs/src/modules/GlobalClientData.ts
+++ nextjs/src/modules/GlobalClientData.ts
@@ -1,2 +1,2 @@
-export const apiURL = "https://ef69-74-88-25-174.ngrok-free.app";
+export const apiURL = "https://ba3b93b51050.ngrok-free.app";
 export const headers = {"ngrok-skip-browser-warning":"true"}
