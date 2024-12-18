d2e2138bc97b0bdef963a2ce68ca1c130ac8e875
Jason Bondarchuk <jason.r.bondarchuk@gmail.com>
2024-12-17 22:53:25 -0500

    small edit

--- nextjs/src/components/Text/Reveal.tsx
+++ nextjs/src/components/Text/Reveal.tsx
@@ -5,7 +5,7 @@ export const Reveal = ({text, size}:{text:string, size:string}) => {
   size = size ? size: "pt-10";
     return (
       <>
-        <h1 className={"overflow-hidden text-white flex justify-center flex-nowrap " + size}>
+        <h1 className={"overflow-hidden flex justify-center flex-nowrap " + size}>
           {text.match(/./gu)!.map((char, index) => (
             <span
               className="animate-text-reveal inline-block [animation-fill-mode:backwards]"
