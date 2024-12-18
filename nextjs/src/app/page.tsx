22e8589857861fc9d1f0ab2acbf2cb83b3ea3866
Jason Bondarchuk <jason.r.bondarchuk@gmail.com>
2024-12-18 17:50:19 -0500

    Animated front page on observe

--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -1,9 +1,11 @@
+'use client';
 import React from 'react';
 import styles from '../styles/Home.module.css';
 import mainStyles from '../styles/Main.module.css';
 import Icons from '../components/IconContainer/Icons';
 import Link from 'next/link';
 import {Reveal} from '../components/Text/Reveal';
+import useIntersectionObserver from '@/modules/IntersectionObserver';
 
 
 const classForHeader = [
@@ -12,6 +14,10 @@ const classForHeader = [
 ];
 
 function Home() {
+  const { ref: portfolioRef, isIntersecting: isPortfolioVisible } = useIntersectionObserver({ threshold: 0.1 });
+  const { ref: otherInfoRef, isIntersecting: isOtherInfoVisible } = useIntersectionObserver({ threshold: 0.1 });
+  const { ref: aboutRef, isIntersecting: isAboutVisible } = useIntersectionObserver({ threshold: 0.1 });
+
     return(
       <div>
           <div className={styles.headerCss}>
@@ -30,8 +36,8 @@ function Home() {
 
         <div className={mainStyles.Main}>
           <div className={mainStyles.section}>
-            <div className="p-1 mb-12 mt-6">
-              <Reveal text="ABOUT ME" size=" text-5xl"></Reveal>
+            <div className="p-1 mb-12 mt-6" ref={aboutRef}>
+              {isAboutVisible && <Reveal text="ABOUT ME" size=" text-5xl"></Reveal>}
             </div>
             <p id={mainStyles.body1} className=" text-justify">
               I am a software engineer from Long Island and MS in Computer Science at Johns Hopkins University. 
@@ -42,8 +48,8 @@ function Home() {
         </div>
         <div className={" bg-slate-300"}>
           <div className={mainStyles.section}>
-          <div className="p-1 mb-12 mt-6">
-              <Reveal text="PORTFOLIO GUIDE" size="text-5xl text-gray-800"></Reveal>
+          <div className="p-1 mb-12 mt-6" ref={portfolioRef}>
+              {isPortfolioVisible && <Reveal text="PORTFOLIO GUIDE" size="text-5xl text-gray-800"></Reveal>}
             </div>
             <p id={mainStyles.body1} className=" text-justify">
               <br/><br/>For a look at a few of my personal projects, please visit the <Link href='/Projects' className='underline hover:text-blue-400'>'Projects'</Link> tab. These are non-exhaustive and do not include confidential projects from previous employment.
@@ -56,8 +62,8 @@ function Home() {
         </div>
         <div className={mainStyles.Main}>
           <div className={mainStyles.section + " flex items-center justify-center flex-col text-center"}>
-            <div className="p-1 mb-12 mt-6">
-              <Reveal text="OTHER INFO" size="text-5xl"></Reveal>
+            <div className="p-1 mb-12 mt-6" ref={otherInfoRef}>
+              {isOtherInfoVisible && <Reveal text="OTHER INFO" size="text-5xl"></Reveal>}
             </div>
             <p id={mainStyles.body1} >
               <br/>I am located in New York State but willing to relocate.
