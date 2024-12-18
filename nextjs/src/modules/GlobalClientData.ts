203f1e4bfa78bb400ee21b5cd9d9a74ac54c7c4a
Jason Bondarchuk <jason.r.bondarchuk@gmail.com>
2024-12-17 22:59:48 -0500

    Forgot to switch to prod

--- nextjs/src/modules/GlobalClientData.ts
+++ nextjs/src/modules/GlobalClientData.ts
@@ -1,4 +1,4 @@
 // export const apiURL = "https://ba3b93b51050.ngrok.app";
-// export const apiURL = "https://app.jasonbondarchuk.tech/api";
-export const apiURL = "http://localhost:80/api";
+export const apiURL = "https://app.jasonbondarchuk.tech/api";
+// export const apiURL = "http://localhost:80/api";
 export const headers = {"ngrok-skip-browser-warning":"true"}
